976d038a41fb4f9e1a8f6182e27bcf77cd396762
M <marilynduf@gmail.com>
2026-01-05 19:35:01 -0500

    feature: fetching data (places)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,23 @@
-import { useSyncExternalStore, useState } from "react";
+import { useSyncExternalStore, useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [AvailablePlaces, setAvailablePlaces] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/places")
+            .then((response) => {
+                return response.json();
+            })
+            .then((resData) => {
+                setAvailablePlaces(resData.places);
+            });
+    }, []);
+
     return (
         <Places
             title="Available Places"
-            places={[]}
+            places={AvailablePlaces}
             fallbackText="No places available."
             onSelectPlace={onSelectPlace}
         />
